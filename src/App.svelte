cc230440663564ceb6e6169c4c8c6bdfe61258ec
Alex Alan Nunes <alexalannunes@gmail.com>
2025-05-12 15:26:47 -0300

    Remove unused imports and component references from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
   import Calculator from "./components/calculator/calculator.svelte";
   import Todo from "./components/todo/todo.svelte";
   import Autocomplete from "./components/autocomplete/autocomplete.svelte";
 </script>
 
 <main>
-  <Counter />
   <Calculator />
   <Todo />
   <Autocomplete />
